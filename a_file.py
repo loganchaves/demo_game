47dcc5ef35b382e91ffa3c586b6f55263c3ab38c
Nicholas Coxe <autumncoxe@Nicholass-MacBook-Pro.local>
2019-02-07 15:06:14 -0500

    the toilet survived

--- a_file.py
+++ a_file.py
@@ -11,18 +11,30 @@ while True:
     if option == "1":
         print('a) taco bell:\n' +
               'b) Burger King\n' +
-              'c)Arbys\n')
+              'c) Arbys\n')
         resopt = input("Choose the restaurant: ")
 
-        if resopt =="a":
+        if resopt == "b":
+            print('a) salad\n'+
+                  'b) chicken sandwich\n'+
+                  'c) whopper\n')
+            burger = input('what you want: ')
+            if burger == "a":
+                print('You got foot lettuce')
+            if burger == "b":
+                print('The burger KING... blasts out from the back to roundhouse you in the face')
+            if burger == "c":
+                print('You have a exquisite lunch')
+
+        if resopt == "a":
             print('a) soft\n' +
                   'b) hard\n'  +
                   'c) nacho\n')
             taco = input('type of taco: ')
             if taco == 'a':
-                print ('ya got diarrhea')
+                print('ya got diarrhea')
             if taco == 'b':
-                print ('ya got diarrhea')
+                print('ya got diarrhea')
             if taco == 'c':
                 print('your toilet survived')
 
