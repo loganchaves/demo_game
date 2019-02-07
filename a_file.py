fb1ebf7506f09d623138ae489f954e66c3acd673
Logan Chaves <loganchaves@icloud.com>
2019-02-07 12:22:47 -0800

    YO

--- a_file.py
+++ a_file.py
@@ -13,6 +13,21 @@ while True:
               'b) Burger King\n' +
               'c) Arbys\n')
         resopt = input("Choose the restaurant: ")
+        if resopt == 'c':
+            print(
+                'a) Gyro\n'+
+                'b) roast beef\n'+
+                'c) vegan sandwich\n')
+            meat = input('what ya want: ')
+            
+            if meat == 'a':
+                print('FAIL idk im not greek')
+            if meat == 'b':
+                print('WIN you came to the right place my guy')
+            if meat == 'c':
+                print('FAIL why are you here we have the meats')
+
+
 
         if resopt == "b":
             print('a) salad\n'+
@@ -20,11 +35,11 @@ while True:
                   'c) whopper\n')
             burger = input('what you want: ')
             if burger == "a":
-                print('You got foot lettuce')
+                print('FAIL You got foot lettuce')
             if burger == "b":
-                print('The burger KING... blasts out from the back to roundhouse you in the face')
+                print('FAIL The burger KING... blasts out from the back to roundhouse you in the face')
             if burger == "c":
-                print('You have a exquisite lunch')
+                print('YOU WIN You have a exquisite lunch')
 
         if resopt == "a":
             print('a) soft\n' +
@@ -32,11 +47,11 @@ while True:
                   'c) nacho\n')
             taco = input('type of taco: ')
             if taco == 'a':
-                print('ya got diarrhea')
+                print('FAIL ya got diarrhea')
             if taco == 'b':
-                print('ya got diarrhea')
+                print('FAIL ya got diarrhea')
             if taco == 'c':
-                print('your toilet survived')
+                print('YOU WIN your toilet survived')
 
                 break
 
