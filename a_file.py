c86ab6d042c193442a7d93b7b73b4c6bf7725c33
Nicholas Coxe <autumncoxe@Nicholass-MacBook-Pro.local>
2019-02-07 14:29:28 -0500

    game

--- a_file.py
+++ a_file.py
@@ -1,4 +1,18 @@
-#hi
+students = []
+
+while True:
+
+    print("FOOD:\n" +
+          "1) Start\n" +
+          "2) Exit\n")
+
+    option = input("\n> ")
+
+    if option == "1":
+        option = input("Choose the restaurant: ")
+        print('a) taco bell:\n' +
+              'b) Burger King\n'+
+              'c)Arbys\n')
 
 
 
